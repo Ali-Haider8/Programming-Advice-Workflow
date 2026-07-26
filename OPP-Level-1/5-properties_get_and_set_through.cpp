1c4b26c185269261083cb901eb47b6923c8b74ae
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-07-26 22:05:46 +0300

    write example

---
+++ OPP-Level-1/5-properties_get_and_set_through.cpp
@@ -0,0 +1,34 @@
+// created by ali_h at 2026-07-26 21:57
+
+#include <iostream>
+
+using namespace std;
+
+class clsPerson {
+  private:
+    string _FirstName;
+
+  public:
+    void SetFirstName(string FirstName) {
+        _FirstName = FirstName;
+    }
+
+    string GetFirstName() {
+        return _FirstName;
+    }
+
+    __declspec(property(get = GetFirstName, put = SetFirstName)) string FirstName;
+};
+
+int main() {
+    clsPerson Person1;
+
+    Person1.SetFirstName("Mohammed");
+    cout << Person1.GetFirstName() << endl;
+
+    // instead of the above we only write this
+    Person1.FirstName = "Mohammed";
+
+    system("pause>0");
+    return 0;
+}
