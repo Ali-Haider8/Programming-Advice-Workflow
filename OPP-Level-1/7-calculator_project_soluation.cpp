f39fc762c18bbeb728fbca2db23c96ac308562b8
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-07-29 09:10:46 +0300

    write solution

--- OPP-Level-1/7-calculator_project_soluation.cpp
+++ OPP-Level-1/7-calculator_project_soluation.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class clsCalculator {
 
-    private:
+  private:
     float _Result = 0;
     float _LastNumber = 0;
     string _LastOperation = "Clear";
@@ -16,8 +16,7 @@ class clsCalculator {
         return (Number == 0);
     }
 
-    public:
-
+  public:
     void Add(float Number) {
         _LastNumber = Number;
         _PreviousResult = _Result;
@@ -34,6 +33,8 @@ class clsCalculator {
 
     void Divide(float Number) {
         _LastNumber = Number;
+        if (_IsZero(Number))
+            Number = 1;
         _PreviousResult = _Result;
         _LastOperation = "Dividing";
         _Result /= Number;
@@ -53,11 +54,49 @@ class clsCalculator {
     void Clear() {
         _LastNumber = 0;
     }
+
+    void CancelLastOperation() {
+        _LastNumber = 0;
+        _LastOperation = "Cancelling Last Operation";
+        _Result = _PreviousResult;
+    }
+
+    void PrintResult() {
+        cout << "Result ";
+        cout << "After " << _LastOperation << " " << _LastNumber << " is: " << _Result << endl;
+    }
 };
 
 int main() {
 
-    
+    clsCalculator Calculator1;
 
+    Calculator1.Clear();
+
+    Calculator1.Add(10);
+    Calculator1.PrintResult();
+
+    Calculator1.Add(100);
+    Calculator1.PrintResult();
+
+    Calculator1.Subtract(20);
+    Calculator1.PrintResult();
+
+    Calculator1.Divide(0);
+    Calculator1.PrintResult();
+
+    Calculator1.Divide(2);
+    Calculator1.PrintResult();
+
+    Calculator1.Multiply(3);
+    Calculator1.PrintResult();
+
+    Calculator1.CancelLastOperation();
+    Calculator1.PrintResult();
+
+    Calculator1.Clear();
+    Calculator1.PrintResult();
+
+    system("pause>0");
     return 0;
 }
