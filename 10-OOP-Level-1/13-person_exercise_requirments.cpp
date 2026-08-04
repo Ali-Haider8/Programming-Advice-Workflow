86e3320e4bfe332dbea442bbb30a4111859a1e7c
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-08-04 16:44:14 +0300

    complete class code

--- 10-OOP-Level-1/13-person_exercise_requirments.cpp
+++ 10-OOP-Level-1/13-person_exercise_requirments.cpp
@@ -15,18 +15,18 @@ class clsPerson {
 
   public:
     clsPerson(int ID, string FirstName, string LastName, string Email, int PhoneNumber) {
-        ID = _ID;
-        FirstName = _FirstName;
-        LastName = _LastName;
-        Email = _Email;
-        PhoneNumber = _PhoneNumber;
+        _ID = ID;
+        _FirstName = FirstName;
+        _LastName = LastName;
+        _Email = Email;
+        _PhoneNumber = PhoneNumber;
     }
 
     void SetID(int ID) {
-        _ID= ID;
+        _ID = ID;
     }
 
-    int ID(){
+    int ID() {
         return _ID;
     }
 
@@ -34,27 +34,74 @@ class clsPerson {
         _FirstName = FirstName;
     }
 
-    void setLastName(string LastName) {
-
+    string FirstName() {
+        return _LastName;
     }
 
+    void setLastName(string LastName) {
+        _LastName = LastName;
+    }
 
+    string LastName() {
+        return _LastName;
+    }
+
+    void setEmail(string Email) {
+        _Email = Email;
+    }
+
+    string Email() {
+        return _Email;
+    }
+
+    string FullName() {
+        return (_FirstName + " " + _LastName);
+    }
+
+    void setPhoneNumber(int PhoneNumber) {
+        _PhoneNumber = PhoneNumber;
+    }
+
+    int PhoneNumber() {
+        return _PhoneNumber;
+    }
+
+    void SendEmail(string subject, string body) {
+        cout << "\n";
+        cout << "The following message sent successfully to email: " << _Email << endl;
+        cout << "Subject: " << subject << endl;
+        cout << "Body: " << body << endl;
+    }
+
+    void SendSMS(string message) {
+        cout << "\n";
+        cout << "The following message sent successfully to email: " << _Email << endl;
+        cout << message;
+        cout << "\n";
+    }
 
     void Print() {
-        cout << "Info" << endl;
+        cout << "Info:" << endl;
+        cout << "______________________________" << endl;
         cout << "ID: " << _ID << endl;
         cout << "FirstName: " << _FirstName << endl;
         cout << "LastName: " << _LastName << endl;
-        cout << "Full Name: " << _FullName << endl;
+        cout << "Full Name: " << _FirstName << " " << _LastName << endl;
         cout << "Email: " << _Email << endl;
         cout << "Phone: " << _PhoneNumber << endl;
+        cout << "______________________________" << endl;
+        cout << "\n";
     }
 };
 
 int main() {
 
-    clsPerson Person1(10, "Ali", "Nukhailawi", "ali@gmail.com", 123);
+    clsPerson Person1(10, "Ali", "Nukhailawi", "ali@gmail.com", 136);
     Person1.Print();
 
+    Person1.SendEmail("Hi", "How are you?");
+    Person1.SendSMS("How are you?");
+
+    system("pause>0");
     return 0;
 }
