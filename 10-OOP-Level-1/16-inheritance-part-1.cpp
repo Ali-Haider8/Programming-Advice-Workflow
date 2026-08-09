bdc00ab0739d27ff27b3e10e769f0b7774a3b5aa
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-08-09 23:35:35 +0300

    write some code

--- 10-OOP-Level-1/16-inheritance-part-1.cpp
+++ 10-OOP-Level-1/16-inheritance-part-1.cpp
@@ -4,9 +4,131 @@
 
 using namespace std;
 
-int main() {
+class clsPerson {
+  private:
+    int _ID;
+    string _FirstName;
+    string _LastName;
+    string _Email;
+    string _Phone;
 
-    
+  public:
+    // we put default constructor here temporarily because inheritance will use it
+    // in the coming lectures we will solve the parmtaired constructor with inheritance.
+    clsPerson() {
+    }
+    clsPerson(int ID, string FirstName, string LastName, string Email, string Phone) {
+        _ID = ID;
+        _FirstName = FirstName;
+        _LastName = LastName;
+        _Email = Email;
+        _Phone = Phone;
+    }
+
+    // Read only Property
+    int ID() {
+        return _ID;
+    }
+
+    // Property Set
+    void setFirstName(string FirstName) {
+        _FirstName = FirstName;
+    }
+
+    // Property Get
+    string FirstName() {
+        return _FirstName;
+    }
+
+    // Property Set
+    void setLastName(string LastName) {
+        _LastName = LastName;
+    }
+
+    // Property Get
+    string LastName() {
+        return _LastName;
+    }
+
+    string FullName() {
+        return (_FirstName + " " + _LastName);
+    }
+
+    // Property Set
+    void setEmail(string Email) {
+        _Email = Email;
+    }
+
+    // Property Get
+    string Email() {
+        return _Email;
+    }
+
+    // Property Set
+    void setPhone(string Phone) {
+        _Phone = Phone;
+    }
+
+    // Property Get
+    string Phone() {
+        return _Phone;
+    }
+
+    void Print() {
+        cout << "Info: " << endl;
+        cout << "______________________" << endl;
+        cout << "ID: " << _ID << endl;
+        cout << "FirstName: " << _FirstName << endl;
+        cout << "LastName: " << _LastName << endl;
+        cout << "FullName: " << FullName() << endl;
+        cout << "Email: " << _Email << endl;
+        cout << "Phone: " << _Phone << endl;
+        cout << "______________________" << endl;
+        cout << "\n";
+    }
+
+    void SendEmail(string Subject, string Body) {
+        cout << "Sending Email to: " << _Email << endl;
+        cout << "Subject: " << Subject << endl;
+        cout << "Body: " << Body << endl;
+    }
+
+    void SendSMS(string Body) {
+        cout << "Sending SMS to: " << _Phone << endl;
+        cout << "Body: " << Body << endl;
+    }
+
+};
+
+class clsEmployee : public clsPerson {
+    private:
+    string _Title;
+    string _Department;
+    float _Salary;
+
+    public:
+
+    // Property Set
+    void setTitle(string Title){
+        _Title = Title;
+    }
+
+    // Property Get
+    string Title(){
+        return _Title;
+    }
+
+    // Property Set
+    void setDepartment(string Department){
+        _Department = Department;
+    }
+
+    //
+
+
+}
+
+int main() {
 
     return 0;
 }
